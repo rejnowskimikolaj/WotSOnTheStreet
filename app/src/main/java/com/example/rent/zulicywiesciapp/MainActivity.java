cedad34d7d03515a26439cb4f759eac5a321cc25
Mikolaj Rejnowski <rejnowski.mikolaj@gmail.com>
2017-03-07 13:39:45 +0100

    Toolbar updated: actions

--- app/src/main/java/com/example/rent/zulicywiesciapp/MainActivity.java
+++ app/src/main/java/com/example/rent/zulicywiesciapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rent.zulicywiesciapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
