383b8557b17ccc6aa7867b0ddaf3c266dd0e52e9
Marcin Dudek <marcin.a.dudek@gmail.com>
2017-03-08 15:27:42 +0100

    backend add NewsItem and NeswItemList to client

--- client/app/src/main/java/com/example/rent/zulicywiesciapp/model/NewsItem.java
+++ client/app/src/main/java/com/example/rent/zulicywiesciapp/model/NewsItem.java
@@ -1,31 +1,83 @@
 package com.example.rent.zulicywiesciapp.model;
 
+import java.util.Date;
+
 /**
  * Created by User on 2017-03-08.
  */
 
 public class NewsItem {
-    String title;
-    String content;
-    String url;
+    private Long id;
+    private String title;
+    private String content;
+    private Integer priority;
+    private String author;
+    private Date date;
+    private String img_url;
+
+    public NewsItem(Long id, String title, String content, Integer priority, String author, Date date, String img_url) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.priority = priority;
+        this.author = author;
+        this.date = date;
+        this.img_url = img_url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getContent() {
         return content;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public NewsItem(String title, String content, String url) {
-        this.title = title;
+    public void setContent(String content) {
         this.content = content;
-        this.url = url;
     }
 
+    public Integer getPriority() {
+        return priority;
+    }
 
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
 }
---
+++ client/app/src/main/java/com/example/rent/zulicywiesciapp/model/NewsItemList.java
@@ -0,0 +1,24 @@
+package com.example.rent.zulicywiesciapp.model;
+
+import java.util.List;
+
+/**
+ * Created by md on 3/8/17.
+ */
+
+public class NewsItemList {
+
+    private List<NewsItem> news;
+
+    public NewsItemList(List<NewsItem> news) {
+        this.news = news;
+    }
+
+    public List<NewsItem> getNews() {
+        return news;
+    }
+
+    public void setNews(List<NewsItem> news) {
+        this.news = news;
+    }
+}
