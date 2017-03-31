8ed2141e8adfabbd27d0449436deba6759084a29
Mikolaj Rejnowski <rejnowski.mikolaj@gmail.com>
2017-03-31 19:34:31 +0200

    implemented adding news, TODO: path of the imeage from uri

--- app/src/main/java/com/example/rent/zulicywiesciapp/AddNewsActivity.java
+++ app/src/main/java/com/example/rent/zulicywiesciapp/AddNewsActivity.java
@@ -1,8 +1,11 @@
 package com.example.rent.zulicywiesciapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
@@ -16,18 +19,24 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.rent.zulicywiesciapp.model.AddNewsDTO;
 import com.example.rent.zulicywiesciapp.model.AddNewsResponse;
 import com.example.rent.zulicywiesciapp.model.Category;
 import com.example.rent.zulicywiesciapp.retrofit.ApiManager;
 import com.example.rent.zulicywiesciapp.utils.CategoryUtil;
+import com.example.rent.zulicywiesciapp.utils.SessionManager;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -87,8 +96,44 @@ public class AddNewsActivity extends AbstractCapsuleActivity  implements ApiMana
     @OnClick(R.id.add_button)
     void onAddButtonClicked(){
 
-        //AddNewsDTO dto = new AddNewsDTO(titleInput.getText().toString(),contentInput.getText().toString(),imageUri.getPath()})
+        SessionManager.checkIfLoggedIn(this);
+        Set<Integer>  categorySet = categories.keySet();
+        List<Integer> categoryList = new ArrayList<>();
+
+        for(Integer i: categorySet){
+            categoryList.add(i);
+        }
+
+        File image = new File(imageUri.getPath());
+
+
+        AddNewsDTO dto = new AddNewsDTO(titleInput.getText().toString()
+                                        ,contentInput.getText().toString()
+                                        ,priority
+                                        ,getRealPathFromUri(imageUri)
+                                        ,categoryList);
+        ApiManager.addNews(SessionManager.getInstance().getUser().getToken(),dto,this);
     }
+
+    public  String getRealPathFromUri(Uri uri) {
+        String result = "";
+        String documentID;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            String[] pathParts = uri.getPath().split("/");
+            documentID = pathParts[pathParts.length - 1];
+        } else {
+            String pathSegments[] = uri.getLastPathSegment().split(":");
+            documentID = pathSegments[pathSegments.length - 1];
+        }
+        String mediaPath = MediaStore.Images.Media.DATA;
+        Cursor imageCursor = getContentResolver().query(uri, new String[]{mediaPath}, MediaStore.Images.Media._ID + "=" + documentID, null, null);
+        if (imageCursor.moveToFirst()) {
+            result = imageCursor.getString(imageCursor.getColumnIndex(mediaPath));
+        }
+        return result;
+    }
+
+
 
     private void setSeekBar(){
 
@@ -169,14 +214,12 @@ public class AddNewsActivity extends AbstractCapsuleActivity  implements ApiMana
 
         int id = CategoryUtil.getCategoryIdFromString(categoryName);
         categories.put(id,new Category(id,categoryName));
-        Log.d("ADDNEWSACTIVITY", "addCategoryToList: "+categories.toString());
 
     }
 
     private void deleteCategoryFromList(String categoryName) {
         int id = CategoryUtil.getCategoryIdFromString(categoryName);
         categories.remove(id);
-        Log.d("ADDNEWSACTIVITY", "deleteCategoryfromList: "+categories.toString());
 
     }
 
@@ -226,8 +269,11 @@ public class AddNewsActivity extends AbstractCapsuleActivity  implements ApiMana
                 REQUEST_GALLERY_IMAGE);
     }
 
+
+
     @Override
     public void onNewsAdded(AddNewsResponse response) {
-
+        Log.d("ADD NEWS", "onNewsAdded: " +response);
+        Toast.makeText(this,"response: "+response,Toast.LENGTH_SHORT).show();
     }
 }
