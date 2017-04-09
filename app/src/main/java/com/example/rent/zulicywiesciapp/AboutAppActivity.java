4577406ac1b80165be4ae635bebe12d0a4b63731
rejnowskimikolaj <rejnowski.mikolaj@gmail.com>
2017-04-09 16:44:10 +0200

    Implemented AboutAppActivity, finished it's layout

--- app/src/main/java/com/example/rent/zulicywiesciapp/AboutAppActivity.java
+++ app/src/main/java/com/example/rent/zulicywiesciapp/AboutAppActivity.java
@@ -1,7 +1,10 @@
 package com.example.rent.zulicywiesciapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AboutAppActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class AboutAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+       // getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setTitle(getString(R.string.about_app));
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onFabClicked();
+            }
+        });
+
+    }
+
+    private void onFabClicked() {
+        finish();
     }
 }
